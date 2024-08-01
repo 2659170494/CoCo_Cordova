95d7e27701604d4726bc232dd14c1efe284c2848
GreenYoshi (Wang Han) <13178026480@163.com>
2024-08-01 07:13:05 +0000

    添加对于cordova_android@9的java判断hook和android_sdk的dx修复的hook

---
+++ hooks/before_build.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const util = require('util');
+const stat = util.promisify(fs.stat);
+const path = require('path');
+
+module.exports = function(ctx) {
+
+    // const platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
+    // const apkFileLocation = path.join(platformRoot, 'build/outputs/apk/android-debug.apk');
+    try{
+        // Make sure android platform is part of build
+        if (!ctx.opts.platforms.includes('android')) return;
+    
+        var ANDROID_SDK_PATH = process.env.ANDROID_SDK_ROOT;
+        if (!ANDROID_SDK_PATH || ANDROID_SDK_PATH == "" || ANDROID_SDK_PATH == undefined){
+            var ANDROID_SDK_PATH  = process.env.ANDROID_HOME;
+            if (!ANDROID_SDK_PATH || ANDROID_SDK_PATH == "" || ANDROID_SDK_PATH == undefined) throw "CANNOT_FIND_ANDROID_SDK"
+            console.log(`(ANDROID_HOME)ANDROID_SDK_PATH=${ANDROID_SDK_PATH}`)
+        }else{
+            console.log(`(ANDROID_SDK_ROOT)ANDROID_SDK_PATH=${ANDROID_SDK_PATH}`)
+        }
+        const android_sdk_buildtool_path = path.join(ANDROID_SDK_PATH, 'build-tools');
+
+        let dir_paths = fs.readdirSync(android_sdk_buildtool_path);
+        var android_sdk_buildtool_new_path = "";
+        console.log(dir_paths.sort()[dir_paths.length - 1].split(".")[0])
+        if (Number(dir_paths.sort()[dir_paths.length - 1].split(".")[0]) > 30) android_sdk_buildtool_new_path = path.join(android_sdk_buildtool_path, dir_paths.sort()[dir_paths.length - 1]);
+        if (android_sdk_buildtool_new_path=="") throw "Cannot_Find_Newest_BuildTools_Version";
+        console.log(android_sdk_buildtool_new_path);
+
+        return stat(apkFileLocation).then(stats => {
+          console.log(`Size of ${apkFileLocation} is ${stats.size} bytes`);
+        });
+    }catch(err){
+        console.log(err);
+        throw err;
+    }
+};
+
---
+++ hooks/install_android9.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const util = require('util');
+const stat = util.promisify(fs.stat);
+const path = require('path');
+
+module.exports = function(ctx) {
+    try{
+
+        java_version = "11.x"
+
+        // Make sure android platform is part of build
+        //if (!ctx.opts.platforms.includes('android')) return;
+        const cordova_android_version_path = path.resolve('node_modules/cordova-android/package.json');
+        const java_version_delection_modules = path.resolve('node_modules/cordova-android/bin/templates/cordova/lib/check_reqs.js');
+        const java_version_delection_platform = path.resolve('platforms/android/cordova/lib/check_reqs.js');
+        
+        let version_json = fs.readFileSync(cordova_android_version_path, {encoding: 'utf-8'});
+        var jsonParsed = JSON.parse(version_json); 
+        //console.log(jsonParsed);
+        var module_version = String(jsonParsed["version"].toString());
+        //console.log(module_version);
+        if (module_version.split(".")[0] !== "9") return;
+        let module_js = fs.readFileSync(java_version_delection_modules, {encoding: 'utf-8'});
+        let platform_js = fs.readFileSync(java_version_delection_platform, {encoding: 'utf-8'});
+        
+        module_js = module_js.replace("const EXPECTED_JAVA_VERSION = '1.8.x';",`const EXPECTED_JAVA_VERSION = '${java_version}';`)
+        platform_js = platform_js.replace("const EXPECTED_JAVA_VERSION = '1.8.x';",`const EXPECTED_JAVA_VERSION = '${java_version}';`)
+        
+        fs.writeFileSync(java_version_delection_modules, module_js);
+        fs.writeFileSync(java_version_delection_platform, platform_js);
+        
+        
+        // return stat(apkFileLocation).then(stats => {
+        //   console.log(`Size of ${apkFileLocation} is ${stats.size} bytes`);
+        // });
+    }catch(err){
+        console.log(err);
+        throw err;
+    }
+};
+
